6b9a1c59350ce5728e637c5eed005f3ff9712bea
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-29 11:34:07 +0530

    Add configurations to fix cross requests

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,14 @@ module BookAppointmentBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # handle cross connections
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins /\Ahttp:\/\/localhost:\d+\z/
+        resource '*', headers: :any, methods: :any
+      end
+    end
+
   end
 end
