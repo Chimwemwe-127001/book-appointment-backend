7a4496bf6d212c3910ffd0e17545c1a4d66b8b3c
Anselem <anselemodims@gmail.com>
2022-04-30 08:45:25 +0100

    Feat: setup devise and include seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.first_or_create!(email: 'johndoe@gmmail.com', password:'123456', password_confirmation: '123456', role: User.roles[:admin])
