ff50820210fe23891da6f8d61be27796bcc98409
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-03 02:00:50 +0530

    Add doctors table column

--- db/migrate/20220502202641_create_doctors.rb
+++ db/migrate/20220502202641_create_doctors.rb
@@ -1,7 +1,12 @@
 class CreateDoctors < ActiveRecord::Migration[7.0]
   def change
     create_table :doctors do |t|
-
+      t.string :name
+      t.text :details
+      t.string :photo
+      t.string :city
+      t.string :specialization
+      t.decimal :cost
       t.timestamps
     end
   end
