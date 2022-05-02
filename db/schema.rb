b136462fcc8e78c641405a16c52134f810ee1235
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-03 02:03:05 +0530

    Create migrations for doctors

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_30_100914) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_02_202641) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "name"
+    t.text "details"
+    t.string "photo"
+    t.string "city"
+    t.string "specialization"
+    t.decimal "cost"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "oauth_access_tokens", force: :cascade do |t|
     t.bigint "resource_owner_id"
