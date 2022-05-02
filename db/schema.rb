9b1349e03a1e3bd7a61b93538e91427c0362633d
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-03 01:56:07 +0530

    Delete previous files

--- db/schema.rb
+++ db/schema.rb
@@ -14,17 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_30_100914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "doctors", force: :cascade do |t|
-    t.string "name"
-    t.string "specialty"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "address"
-    t.integer "phone"
-    t.string "email"
-    t.string "image"
-  end
-
   create_table "oauth_access_tokens", force: :cascade do |t|
     t.bigint "resource_owner_id"
     t.bigint "application_id", null: false
