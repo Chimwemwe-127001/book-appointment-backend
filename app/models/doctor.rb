eebcaf62e2fdd282ae048d2baa4bd3e0207d8c0c
Buyaki <rittahbuyaki@gmail.com>
2022-05-04 22:46:26 +0300

    Add reservations model and their relationships

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Doctor < ApplicationRecord
+  has_many :reservations, dependent: :destroy
+  has_many :users, through: :reservations
+
   validates :name, presence: true, length: { in: 1..50 }
   validates :details, presence: true, length: { in: 1..500 }
   validates :photo, presence: true, length: { in: 1..200 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :reservations, dependent: :destroy
+  has_many :doctors, through: :reservations
 
   validates :name, presence: true
   validates :email, format: URI::MailTo::EMAIL_REGEXP
