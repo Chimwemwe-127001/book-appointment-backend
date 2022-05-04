be88b63528354f856005d1af07608c7a1a7a67fc
Buyaki <rittahbuyaki@gmail.com>
2022-05-04 22:50:10 +0300

    Add validations for the reservations table

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :doctor
+
+  validates :city, presence: true, length: { in: 1..50 }
+  validates :date, presence: true, length: { in: 1..20 }
 end
