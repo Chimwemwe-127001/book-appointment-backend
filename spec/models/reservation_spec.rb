112dae794c471371ee05bb023afcca368162ed1e
Buyaki <rittahbuyaki@gmail.com>
2022-05-07 14:37:50 +0300

    Add test for date in the Reservations table

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Reservation, type: :model do
   describe 'Reservation model' do
     user = User.create(name: 'Ritta Sweta', email: 'ritta@example.com', password: '123456')
     doctor = Doctor.new(
-      user_id: user,
       name: 'Dr. Martin',
       details: 'In the past 20 years I have been a doctor for the heart',
       photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
@@ -24,6 +23,16 @@ RSpec.describe Reservation, type: :model do
 
     it 'check if the city is not exceeding 50 characters' do
       subject.city = 'Hello world Hello world Hello world Hello world Hello world Hello world'
+      expect(subject).to_not be_valid
+    end
+
+    it 'check the date is not blank' do
+      subject.date = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'check if the date is not exceeding 20 characters' do
+      subject.date = 'Hello world Hello world Hello world Hello world'
       expect(subject).to_not be_valid
     end
   end
