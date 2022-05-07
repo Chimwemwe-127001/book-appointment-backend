fae7a5b9820357f97f677e1a30213689f9405e1c
Buyaki <rittahbuyaki@gmail.com>
2022-05-07 13:03:44 +0300

    Add tests for Doctor Model to check for name & cost

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -3,5 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe Doctor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Doctor model' do
+    user = User.create(name: 'Ritta Sweta', email: 'ritta@gmail.com', password: '123456')
+    subject { Doctor.new(user_id: user, name: 'Dr. Martin', details: '', photo: '', city: '', specialization: '', cost: 235) }
+    before { subject.save }
+
+    it 'check the name is not blank' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'check if the name is not exceeding 50 characters' do
+      subject.name = 'Hello world Hello world Hello world Hello world Hello world Hello world'
+      expect(subject).to_not be_valid
+    end
+
+    it 'check the cost is not blank' do
+      subject.cost = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'check if cost is numeric' do
+      subject.cost = 'not-numeric'
+      expect(subject).to_not be_valid
+    end
+
+    it 'check if cost is equal or greater than one' do
+      expect(subject.cost).to be >= 1
+    end
+  end
 end
