60375f9352c5ad364069e0665daf0efaee88d2ce
Buyaki <rittahbuyaki@gmail.com>
2022-05-07 13:10:24 +0300

    Add test for details in the Doctor Model

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe Doctor, type: :model do
   describe 'Doctor model' do
     user = User.create(name: 'Ritta Sweta', email: 'ritta@gmail.com', password: '123456')
-    subject { Doctor.new(user_id: user, name: 'Dr. Martin', details: '', photo: '', city: '', specialization: '', cost: 235) }
+    subject { Doctor.new(user_id: user, name: 'Dr. Martin', details: 'In the past 20 years I have been a doctor for the heart', photo: '', city: 'Delhi, India', specialization: 'Carthodologist', cost: 235) }
     before { subject.save }
 
     it 'check the name is not blank' do
@@ -15,6 +15,24 @@ RSpec.describe Doctor, type: :model do
 
     it 'check if the name is not exceeding 50 characters' do
       subject.name = 'Hello world Hello world Hello world Hello world Hello world Hello world'
+      expect(subject).to_not be_valid
+    end
+
+    it 'check the details is not blank' do
+      subject.details = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'check if the name is not exceeding 500 characters' do
+      subject.details = 'Hello world Hello world Hello world Hello world Hello world Hello world
+      Hello world Hello world Hello world Hello world Hello world Hello world
+      Hello world Hello world Hello world Hello world Hello world Hello world
+      Hello world Hello world Hello world Hello world Hello world Hello world
+      Hello world Hello world Hello world Hello world Hello world Hello world
+      Hello world Hello world Hello world Hello world Hello world Hello world
+      Hello world Hello world Hello world Hello world Hello world Hello world
+      Hello world Hello world Hello world Hello world Hello world Hello world
+      Hello world Hello world Hello world Hello world Hello world Hello world'
       expect(subject).to_not be_valid
     end
 
